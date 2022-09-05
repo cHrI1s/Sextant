37a8babb0ab8df752c72b0e5c2363402765e43a5
cHrI1s <crunc2beat@gmail.com>
2022-09-05 20:30:20 +0530

    Module3

---
+++ sextant/src/components/IpDisplayer.js
@@ -0,0 +1,44 @@
+import React from "react";
+import axios from "axios";
+
+class IpDisplayer extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			ipv4: null,
+			ipv6: null,
+			loading: true
+		}
+	}
+	
+	getIpAddress(){
+		axios.get("https://api64.ipify.org/?format=json").then(result=>{
+			const RESPONSE = result.data;
+			this.setState({
+				ipv6: RESPONSE.ip
+			});
+		});
+		
+		axios.get("https://api.ipify.org/?format=json").then(result=>{
+			const RESPONSE = result.data;
+			this.setState({
+				ipv4: RESPONSE.ip
+			});
+		});
+	}
+	
+	render(){
+		this.getIpAddress();
+		return (
+			<div className="d-block position-relative text-end mb-4">
+				<div className="d-block position-relative">
+					Your IpV4 is <strong>{this.state.ipv4}</strong>
+				</div>
+				<div className="d-block position-relative">
+					Your IpV6 is <strong>{this.state.ipv6}</strong>
+				</div>
+			</div>
+		);
+	}
+}
+export default IpDisplayer;
--- sextant/src/components/MainContainer.js
+++ sextant/src/components/MainContainer.js
@@ -2,67 +2,66 @@ import React from "react";
 
 // images sections
 import avatar from "../assets/imgs/avatar.jpg";
+import IpDisplayer from "./IpDisplayer";
 
 class MainContainer extends React.Component{
-	constructor(props) {
-		super(props);
-	}
 	render(){
 		return (
-			<div className="d-block position-relative p-3 bg-danger w-100">
+			<div className="d-block position-relative p-3 w-100">
+				<IpDisplayer/>
 				<div className="row">
 					<div className="col-xl-4 col-lg-6 col-md-12 mb-4">
-						<div className="card">
+						<div className="Card">
 							<img src={avatar}
-								 className="card-img-top w-100" alt="Card Image"/>
-							<div className="card-body">
-								<h5 className="card-title fw-bolder">Card title</h5>
-								<p className="card-text">
+								 className="Card-img-top w-100" alt="Card"/>
+							<div className="Card-body">
+								<h5 className="Card-title fw-bolder">Card title</h5>
+								<p className="Card-text">
 									Description
 								</p>
-								<a href="#" className="btn btn-primary">Go to Link</a>
+								<a href="https://google.com" className="btn btn-primary">Go to Link</a>
 							</div>
 						</div>
 					</div>
 					
 					<div className="col-xl-4 col-lg-6 col-md-12 mb-4">
-						<div className="card">
+						<div className="Card">
 							<img src={avatar}
-								 className="card-img-top w-100" alt="Card Image"/>
-							<div className="card-body">
-								<h5 className="card-title fw-bolder">Card title</h5>
-								<p className="card-text">
+								 className="Card-img-top w-100" alt="Card"/>
+							<div className="Card-body">
+								<h5 className="Card-title fw-bolder">Card title</h5>
+								<p className="Card-text">
 									Description
 								</p>
-								<a href="#" className="btn btn-primary">Go to Link</a>
+								<a href="https://google.com" className="btn btn-primary">Go to Link</a>
 							</div>
 						</div>
 					</div>
 					
 					<div className="col-xl-4 col-lg-6 col-md-12 mb-4">
-						<div className="card">
+						<div className="Card">
 							<img src={avatar}
-								 className="card-img-top w-100" alt="Card Image"/>
-							<div className="card-body">
-								<h5 className="card-title fw-bolder">Card title</h5>
-								<p className="card-text">
+								 className="Card-img-top w-100" alt="Card"/>
+							<div className="Card-body">
+								<h5 className="Card-title fw-bolder">Card title</h5>
+								<p className="Card-text">
 									Description
 								</p>
-								<a href="#" className="btn btn-primary">Go to Link</a>
+								<a href="https://google.com" className="btn btn-primary">Go to Link</a>
 							</div>
 						</div>
 					</div>
 					
 					<div className="col-xl-4 col-lg-4 col-md-6 col-sm-12 mb-4">
-						<div className="card">
+						<div className="Card">
 							<img src={avatar}
-								 className="card-img-top w-100" alt="Card Image"/>
-							<div className="card-body">
-								<h5 className="card-title fw-bolder">Card title</h5>
-								<p className="card-text">
+								 className="Card-img-top w-100" alt="Card"/>
+							<div className="Card-body">
+								<h5 className="Card-title fw-bolder">Card title</h5>
+								<p className="Card-text">
 									Description
 								</p>
-								<a href="#" className="btn btn-primary">Go to Link</a>
+								<a href="https://google.com" className="btn btn-primary">Go to Link</a>
 							</div>
 						</div>
 					</div>
